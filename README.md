0ce8294b8c734d01cfa195a9530d02cf337ba686
ptsouth97 <ptsouth97@gmail.com>
2020-02-25 19:48:35 -0500

    created search list to slice df for bcbas

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Overview
 This application's purpose is to analyze the conversion rate of LinkedIn connections to involvement in a promotional drive
 
+This application uses:
+* [Pandas](https://pandas.pydata.org/)
--- app_linkedin.py
+++ app_linkedin.py
@@ -7,9 +7,45 @@ def main():
 
 	file_name = "Connections.csv"
 
-	df = pd.read_csv(file_name)
+	dataframe = pd.read_csv(file_name)
 
-	print(df.head)
+	# print(dataframe.head)
+
+	count_bcbas(dataframe)
+
+
+def count_bcbas(df):
+	''' counts the number of BCBAs and BCaBAs in the dataset'''
+
+	# print(list(df.columns.values))
+	total = len(df)
+	print(total)
+
+	# Create a list of keywords relevant to BCBA positions
+	searchfor = ['BCBA', \
+                 'Bcba', \
+                 'bcba', \
+                 'Behavior Analyst', \
+                 'behavior analyst', \
+                 'Behavior Consultant', \
+                 'Clinical Supervisor', \
+                 'Clinical Manager', \
+                 'Clinical Director', \
+                 'Supervising', \
+                 'Board Certified Behavior Analyst' \
+                 'LBA', \
+                 'B.C.B.A', \
+                 'Clinician', \
+                 'ABA Training Coordinator']
+
+	# Slice rows with positions contains key words in the 'searchfor' list
+	bcba_position =df[ df['Position'].str.contains('|'.join(searchfor), na=False)]
+	
+	print(bcba_position)
+
+	print(len(bcba_position))
+
+	return
 
 
 if __name__ == '__main__':
